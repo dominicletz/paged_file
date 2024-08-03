833699740fc5c8b961cfde9b98ba87c07eafaa8d
Dominic Letz <dominic.letz@berlin.de>
2024-08-03 02:27:25 +0200

    v1.1.2

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule PagedFile.MixProject do
   use Mix.Project
 
-  @version "1.1.1"
+  @version "1.1.2"
   @name "PagedFile"
   @url "https://github.com/dominicletz/paged_file"
   @maintainers ["Dominic Letz"]
